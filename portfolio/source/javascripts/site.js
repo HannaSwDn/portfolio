46fd774cbc08896a7b0b4f3e45fae85809db6ac1
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-12 20:27:23 +0100

    Adding function that opens the menu when user clicks the menu button

--- portfolio/source/javascripts/site.js
+++ portfolio/source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+// setting DOM variables when the browser has been loaded
+window.onload = function() {
+    const menu_btn = document.getElementById('menu_btn')
+    const menu = document.querySelector('#menu')
+
+    menu_btn.addEventListener('click', event => {
+        menu.style.display = 'block'
+    })
+}
