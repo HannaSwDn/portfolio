5ebd4aebe09e481bd2b9dc777586c659fa5722a0
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-13 11:05:58 +0100

    Changing background color of navigation as the user scrolls down the page

--- portfolio/source/javascripts/site.js
+++ portfolio/source/javascripts/site.js
@@ -1,13 +1,14 @@
 // declaring DOM variables
-let menu_btn, menu, li_one, li_two, li_three, li_four
+let menu_btn, menu, li_one, li_two, li_three, li_four, nav
 // setting DOM variables and event listeners when the browser has been loaded
 window.onload = function() {
-    menu_btn = document.getElementById('menu_btn')
+    menu_btn = document.querySelector('#menu_btn')
     menu = document.querySelector('#menu')
     li_one = document.querySelector('#li-one')
     li_two = document.querySelector('#li-two')
     li_three = document.querySelector('#li-three')
     li_four = document.querySelector('#li-four')
+    nav = document.querySelector('#nav')
 
     menu_btn.addEventListener('click', event => {
         openMenu()
@@ -31,6 +32,18 @@ window.onload = function() {
     li_four.addEventListener('click', event => {
         closeMenu()
         setTimeout(function() { window.open('contact.html', '_blank'); }, 820);
+    })
+
+    // Changing background color of navigation when the user scrolls down the page
+    document.addEventListener('scroll', event => {
+        let offset = window.pageYOffset;
+        if (offset > 300) {
+            nav.style.background = 'rgba(0, 0, 0, .8)'
+            nav.style.color = 'white'
+        } else {
+            nav.style.background = 'white'
+            nav.style.color = 'black'
+        }
     })
 }
 
