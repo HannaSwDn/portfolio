ead6f6cf26e7f6a21ac684900118b197c2911727
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-15 10:33:54 +0100

    Adding files to deploy folder

---
+++ deploy/source/javascripts/site.js
@@ -0,0 +1,72 @@
+// declaring DOM variables
+let menu_btn, menu, li_one, li_two, li_three, li_four, nav, projects_header
+// setting DOM variables and event listeners when the browser has been loaded
+window.onload = function() {
+    projects_header = document.querySelector('#projects_header')
+    about_header = document.querySelector('#about_header')
+    nav = document.querySelector('#nav')
+    menu = document.querySelector('#menu')
+    li_one = document.querySelector('#li-one')
+    li_two = document.querySelector('#li-two')
+    li_three = document.querySelector('#li-three')
+    li_four = document.querySelector('#li-four')
+    li_five = document.querySelector('#li-five')
+
+    nav.addEventListener('click', event => {
+        openMenu()
+    })
+
+    li_one.addEventListener('click', event => {
+        closeMenu()
+        setTimeout(function() { window.open('index.html', '_self'); }, 820);
+    })
+
+    li_two.addEventListener('click', event => {
+        closeMenu()
+        setTimeout(function() { window.open('about.html', '_blank'); }, 820);
+    })
+
+    li_three.addEventListener('click', event => {
+        closeMenu()
+        setTimeout(function() { window.open('projects.html', '_blank'); }, 820);
+    })
+
+    li_four.addEventListener('click', event => {
+        closeMenu()
+        setTimeout(function() { window.open('contact.html', '_blank'); }, 820);
+    })
+
+    li_five.addEventListener('click', event => {
+        closeMenu()
+    })
+
+    document.addEventListener('scroll', event => {
+        let offset = window.pageYOffset;
+        menu.style.top = offset + 'px'
+
+        let offset_top = projects_header.offsetTop
+        if (offset + 500 >= offset_top) {
+            projects_header.style.width = '100%'
+        } else {
+            projects_header.style.width = '0%'
+        }
+    })
+}
+
+const openMenu = () => {
+    menu.style.display = 'block'
+    setTimeout(function(){ li_one.style.opacity = '1' }, 10);
+    setTimeout(function(){ li_two.style.opacity = '1' }, 150);
+    setTimeout(function(){ li_three.style.opacity = '1' }, 300);
+    setTimeout(function(){ li_four.style.opacity = '1' }, 450);
+    setTimeout(function(){ li_five.style.opacity = '1' }, 600);
+}
+
+const closeMenu = () => {
+    setTimeout(function(){ li_one.style.opacity = '0' }, 10);
+    setTimeout(function(){ li_two.style.opacity = '0' }, 150);
+    setTimeout(function(){ li_three.style.opacity = '0' }, 300);
+    setTimeout(function(){ li_four.style.opacity = '0' }, 450);
+    setTimeout(function(){ li_five.style.opacity = '0' }, 600);
+    setTimeout(function(){ menu.style.display = 'none' }, 760);
+}
