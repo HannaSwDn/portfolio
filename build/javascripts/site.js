082de466aae649b9f593ec0aecd900d8a1e27695
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-17 20:22:56 +0100

    Adding more projects to projects.yml file

--- build/javascripts/site.js
+++ build/javascripts/site.js
@@ -1,10 +1,12 @@
 // declaring DOM variables
-let menu_btn, menu, li_one, li_two, li_three, li_four, nav
+let menu_btn, menu, li_one, li_two, li_three, li_four, nav, arrows, about_me, form, btn_message
 
-let headers = []
-headers.push()
 // setting DOM variables and event listeners when the browser has been loaded
 window.onload = function() {
+    btn_message = document.querySelector('#btn_message')
+    div_form = document.querySelector('#message')
+    about_me = document.querySelector('#about_me')
+    arrows = document.querySelectorAll('.arrow')
     nav = document.querySelector('#nav')
     menu = document.querySelector('#menu')
     li_one = document.querySelector('#li-one')
@@ -19,22 +21,22 @@ window.onload = function() {
 
     li_one.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('index.html', '_self'); }, 820);
+        setTimeout(function() { window.open('index.html', '_self'); }, 320);
     })
 
     li_two.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('about.html', '_self'); }, 820);
+        setTimeout(function() { window.open('about.html', '_self'); }, 320);
     })
 
     li_three.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('projects.html', '_self'); }, 820);
+        setTimeout(function() { window.open('projects.html', '_self'); }, 320);
     })
 
     li_four.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('contact.html', '_self'); }, 820);
+        setTimeout(function() { window.open('contact.html', '_self'); }, 320);
     })
 
     li_five.addEventListener('click', event => {
@@ -42,25 +44,22 @@ window.onload = function() {
     })
 
     document.addEventListener('scroll', event => {
-        let offset = window.pageYOffset;
+        let offset = window.pageYOffset
         menu.style.top = offset + 'px'
+    })
+
+    btn_message.addEventListener('click', event => {
+        div_form.style.display = 'block'
+        setTimeout(function(){ div_form.style.opacity = '1' }, 20);
     })
 }
 
 const openMenu = () => {
     menu.style.display = 'block'
-    setTimeout(function(){ li_one.style.opacity = '1' }, 10);
-    setTimeout(function(){ li_two.style.opacity = '1' }, 150);
-    setTimeout(function(){ li_three.style.opacity = '1' }, 300);
-    setTimeout(function(){ li_four.style.opacity = '1' }, 450);
-    setTimeout(function(){ li_five.style.opacity = '1' }, 600);
+    setTimeout(function(){ menu.style.height = '100vh' }, 10);
 }
 
 const closeMenu = () => {
-    setTimeout(function(){ li_one.style.opacity = '0' }, 10);
-    setTimeout(function(){ li_two.style.opacity = '0' }, 150);
-    setTimeout(function(){ li_three.style.opacity = '0' }, 300);
-    setTimeout(function(){ li_four.style.opacity = '0' }, 450);
-    setTimeout(function(){ li_five.style.opacity = '0' }, 600);
-    setTimeout(function(){ menu.style.display = 'none' }, 760);
+    menu.style.height = '0vh'
+    setTimeout(function(){ menu.style.display = 'none' }, 300);
 }
