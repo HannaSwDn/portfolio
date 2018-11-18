df27e2ad31017e29efd1fa8aae1aa41d72d1c02a
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-18 16:56:03 +0100

    Adding CV page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,12 @@
 // declaring DOM variables
-let menu_btn, menu, li_one, li_two, li_three, li_four, nav, div_form, about_header
+let menu_btn, menu, li_one, li_two, li_three, li_four, nav, div_form, about_header, bar_first, bar_second, bar_third, bar_fourth
 
 // setting DOM variables and event listeners when the browser has been loaded
 window.onload = function() {
+    bar_first = document.querySelector('#bar_first')
+    bar_second = document.querySelector('#bar_second')
+    bar_third = document.querySelector('#bar_third')
+    bar_fourth = document.querySelector('#bar_fourth')
     about_header = document.querySelector('#about_me_header')
     div_form = document.querySelector('#message')
     nav = document.querySelector('#nav')
@@ -12,6 +16,14 @@ window.onload = function() {
     li_three = document.querySelector('#li-three')
     li_four = document.querySelector('#li-four')
     li_five = document.querySelector('#li-five')
+    li_six = document.querySelector('#li-six')
+
+    setTimeout(function() {
+        bar_first.style.width = '60px'
+        bar_second.style.width = '60px'
+        bar_third.style.width = '40px'
+        bar_fourth.style.width = '40px'
+    }, 1000)
 
     nav.addEventListener('click', event => {
         openMenu()
@@ -19,17 +31,17 @@ window.onload = function() {
 
     li_one.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('index.html', '_self'); }, 320);
+        setTimeout(function() { window.open('index.html', '_self') }, 320);
     })
 
     li_two.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('about.html', '_self'); }, 320);
+        setTimeout(function() { window.open('about.html', '_self') }, 320);
     })
 
     li_three.addEventListener('click', event => {
         closeMenu()
-        setTimeout(function() { window.open('projects.html', '_self'); }, 320);
+        setTimeout(function() { window.open('projects.html', '_self') }, 320);
     })
 
     li_four.addEventListener('click', event => {
@@ -39,6 +51,11 @@ window.onload = function() {
     })
 
     li_five.addEventListener('click', event => {
+        closeMenu()
+        setTimeout(function() { window.open('cv.html', '_blank') }, 320);
+    })
+
+    li_six.addEventListener('click', event => {
         closeMenu()
     })
 
