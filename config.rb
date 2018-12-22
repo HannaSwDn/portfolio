2ebc5c0fe780c3d3060e92621a49165d513ed8a9
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-22 13:08:44 +0100

    Configures config.rb

--- config.rb
+++ config.rb
@@ -1,25 +1,14 @@
-activate :livereload
-
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+activate :livereload do |livereload|
+  livereload.host = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+end
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
 
 # proxy(
 #   '/this-page-has-no-template.html',
@@ -28,21 +17,3 @@ page '/*.txt', layout: false
 #     which_fake_page: 'Rendering a fake page with a local variable'
 #   },
 # )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
